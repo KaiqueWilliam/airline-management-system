f47dfde1ba9b3f04ea26a48154e383471f8f0bc3
KaiqueWilliam <kaiquewilliamsilva@gmail.com>
2025-12-01 23:12:08 -0300

    sistema de vendas e gestão de clientes implementados com grafo, json

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
 
 def create_app():
-    
     app = Flask(__name__)
-    
     app.config['SECRET_KEY'] = 'trabalho-ed2-uneb'
 
-    
     from .routes.admin_routes import admin_bp
     app.register_blueprint(admin_bp)
+    
+    # Adicionado:
+    from .routes.passenger_routes import passenger_bp
+    app.register_blueprint(passenger_bp)
 
     @app.route('/')
     def index():
